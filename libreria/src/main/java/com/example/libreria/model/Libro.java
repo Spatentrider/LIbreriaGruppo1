7f41b6e85a0216ff5a8cf47a81e125fee2ffdb16
Spatentrider <104686281+Spatentrider@users.noreply.github.com>
2025-07-22 16:12:31 +0200

    aggiunti controlli jakarta

--- libreria/src/main/java/com/example/libreria/model/Libro.java
+++ libreria/src/main/java/com/example/libreria/model/Libro.java
@@ -1,6 +1,7 @@
 package com.example.libreria.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,16 @@ public class Libro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "descrizione", nullable = false, length = 100)
+    @NotBlank(message = "Il titolo è obbligatoria")
+    @Column(name = "titolo", nullable = false, length = 100)
+    @Size(min = 2, max = 100, message = "Il titolo deve essere tra 2 e 100 caratteri")
     private String titolo;
+    @NotBlank(message = "L'autore è obbligatoria")
+    @Column(name = "autore", nullable = false, length = 100)
+    @Size(min = 2, max = 100, message = "L'autore deve essere tra 2 e 100 caratteri")
     private String autore;
+    
+    @NotNull(message = "Il prezzo non può essere nullo")
     private double prezzo;
 
     public Libro(String titolo, String autore, double prezzo) {
